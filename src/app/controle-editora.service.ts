0fd3fc08843e275eda332baae4adb042ff2d7bf9
veronikadelima <veronikadelima@gmail.com>
2024-11-06 20:20:17 -0300

    Criação do projeto e Criação e Codificação das Classes Livro e Editoras e dos Controle Service Livro e Editoras

---
+++ src/app/controle-editora.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Editora } from './editora';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// Controle responsável por algumas rotinas relacionadas a Editoras
+export class ControleEditoraService {
+  //Cria um atributo editoras com três elementos. 
+  private editoras: Array<Editora> = [
+    {codEditora: 1, nome: 'Editora_1'},
+    {codEditora: 2, nome: 'Editora_2'},
+    {codEditora: 3, nome: 'Editora_3'},
+  ]
+  constructor() { }
+
+  //Busca e disponibiliza a lista de editoras
+  getEditoras(): Array<Editora> {
+    return this.editoras;
+  }
+
+  //Busca uma editora pelo seu código e retorna o seu nome.
+  getNomeEditora(codEditora: number): string | undefined {
+    const editora = this.editoras.filter(editora => editora.codEditora === codEditora);
+    return editora.length > 0 ? editora[0].nome : undefined;
+  }
+}
---
+++ src/app/controle-livros.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Livro } from './livro';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// Controle responsável por algumas rotinas relacionadas a Livros
+export class ControleLivrosService {
+  // Cria uma base de dados fake para teste 
+  private livros: Array<Livro> = [
+    {codigo: 1, codEditora:3, titulo: 'Um titulo qualquer 1', resumo: 'Um resumo muito importante 1', autores:['Glaber Mascaranha', 'Edgar Mascaranha', 'Delgat Mascaranha']},
+    {codigo: 2, codEditora:1, titulo: 'Um titulo qualquer 2', resumo: 'Um resumo muito importante 2', autores:['Francisco Muller', 'Raysa Muller']},
+    {codigo: 3, codEditora:2, titulo: 'Um titulo qualquer 3', resumo: 'Um resumo muito importante 3', autores:['Rita Belchior', 'Rute Belchior']},
+  ];
+
+  constructor() { }
+  // Função que busca e disponibiliza a lista de livros.
+  obterLivros() {
+    return this.livros;
+  }
+
+  // Função para inclusão de um novo livro
+  incluir(novoLivro: Livro): void {
+    const maiorCodigo = this.livros.length > 0 ? Math.max(...this.livros.map(livro => livro.codigo)) : 0;
+    novoLivro.codigo = maiorCodigo + 1;
+    this.livros.push(novoLivro);
+  }
+
+  // Função para localizar um livro pelo código e excluí-lo.
+  excluir(codigo: number): void {
+    const indice = this.livros.findIndex(livro => livro.codigo === codigo);
+    if (indice > 0) {
+      this.livros.splice(indice, 1);
+    }
+  }
+
+}
