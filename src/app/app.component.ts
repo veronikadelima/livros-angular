c0dbef3a3ccec937ca5e669a017283fb52a5d939
veronikadelima <veronikadelima@gmail.com>
2024-11-08 19:54:42 -0300

    Alteração em AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { LivroListaComponent } from './livro-lista/livro-lista.component';
-import { LivroDadosComponent } from './livro-dados/livro-dados.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    LivroListaComponent,
-    LivroDadosComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // Corrigido para styleUrls (no plural)
